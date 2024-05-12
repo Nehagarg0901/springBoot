c71e16eb766c463992ee1952ffeba53e02e2407c
CO17341-Neha <nehagarg0901@gmail.com>
2024-05-12 17:27:08 +0530

    initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.demo.dao.UserRepository;
+import com.example.demo.entity.User;
+
+@SpringBootApplication(scanBasePackages = "com.example.demo")
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+        UserRepository userRepository = context.getBean(UserRepository.class);
+        
+        User user = new User();
+        user.setFirstName("Neha");
+        user.setCity("Chandigarh");
+        user.setPersonStatus("I am a java coder");
+        User user1 = userRepository.save(user);
+    }
+}
